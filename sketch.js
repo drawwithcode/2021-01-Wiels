ce0affffb6437299fcfeae2dc0a53f28b91af5b4
Wiels <edward.wiels@student.kuleuven.be>
2021-10-04 17:54:16 +0200

    complete animation

--- sketch.js
+++ sketch.js
@@ -1,13 +1,25 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(frameCount*0.1%255,frameCount+20%255,frameCount*2%255);
+  stroke(frameCount%255,frameCount%255,255);
+  translate(frameCount*0.5%windowWidth,frameCount*0.5%windowHeight)
+  rotate(frameCount*0.02);
+  fill((frameCount+150)%255,(frameCount+50)%255,(frameCount+250)%255);
+  square(0,0,frameCount%windowWidth);
+  fill((frameCount+50)%255,(frameCount+100)%255,(frameCount+200)%255);
+  stroke((frameCount+50)%200,(frameCount+100)%200,(frameCount+200)%255);
+  strokeWeight(frameCount*0.1%20);
+  rotate(frameCount*0.01)
+  circle(width/5,frameCount%500,frameCount%800);
+  stroke((frameCount+50)%255,(frameCount+100)%255,(frameCount+200)%255);
+  strokeWeight(((frameCount+10)%200)*0.1);
+  fill((frameCount+50)%200,(frameCount+100)%200,(frameCount+200)%255);
+  circle(frameCount%500,height/5,frameCount%200);
+  fill(frameCount+20%200,frameCount+220%255,frameCount%150)
+
+  // rotateY(frameCount*0.05);
+  // box(frameCount*0.01%400);
 }
